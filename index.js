2c8a00c25dc78477b826b90258f2b16d5d2dd5b0
prithiv26 <itz.prithiv2006@gmail.com>
2025-02-24 23:15:23 +0530

    React frontend deployed at root url

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const app = express()
 
 app.use(cors());
-
+app.use(express.static('dist'))
 app.use((req,res,next) => {
     const now = new Date();
     const formattedDate = now.toLocaleString('en-US', { 
